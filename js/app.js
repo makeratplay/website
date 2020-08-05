7fcabfe7f4ceac368f8b13a163732585c5fdcae3
Michael Hawkins <makeratplay@gmail.com>
2020-08-05 18:20:22 -0500

    update

---
+++ js/app.js
@@ -0,0 +1,35 @@
+var app = angular.module("app", []);
+
+app.controller('AppController', function($http, $scope, $sce) {
+    var vm = this;
+    vm.selectedProject = {};
+    vm.data = [];
+    init();
+
+
+    function init() {
+        getData();
+    }
+
+    function getData() {
+        return $http.get("/data.json").then(
+            function(response) {
+                vm.data = response.data;
+            });
+    }
+
+
+    vm.viewProject = function(project){
+      vm.selectedProject = project;
+
+      $('#projectModal').modal('show');
+    }
+
+    $scope.trustSrc = function(src) {
+      return $sce.trustAsResourceUrl(src);
+    }
+    
+    $scope.movie = {src:"http://www.youtube.com/embed/Lx7ycjC8qjE", title:"Egghead.io AngularJS Binding"};
+
+
+});
